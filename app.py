76002675e593447ec19d14714fc7df9cfde21e94
KennyChung2000 <43977877+KennyChung2000@users.noreply.github.com>
2018-12-06 00:18:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ from linebot.models import *
 
 app = Flask(__name__)
 
-ACCESS_TOKEN= os.environ['ACCESS_TOKEN']
-SECRET= os.environ['CHANNEL_SECRET']
+ACCESS_TOKEN= os.environ['tOR/2oUKn8BjvGQ7io0DnmzT7syR+tTafBPNyNqHW/zt/B5xQEOk1McUfjdub1Gw6E0OIRDGCgF0bMamYbqtJCV4184s9wmL7/tIowDZFMAx/uv/sjseP9SG+28qebRMMxI4W2SxTRC7g/t0Y1Vf3QdB04t89/1O/w1cDnyilFU=
+']
+SECRET= os.environ['0b951648039451063a36ff0d4ea59284']
 
 # Channel Access Token
 line_bot_api = LineBotApi(ACCESS_TOKEN)
